291a4796bd18f2072d09ea0dfb0e1d1b43fa1e4b
Pierre Beaujean <pierreb24@gmail.com>
2019-10-08 08:58:57 +0200

    Create Compteur.java

---
+++ Compteur.java
@@ -0,0 +1,15 @@
+class Compteur {
+  private int num;
+  
+  public Compteur() {
+    num = 0;
+  }
+  
+  public void incr() {
+    num += 1;
+  }
+  
+  public void decr() {
+    /* TODO */
+  }
+}
